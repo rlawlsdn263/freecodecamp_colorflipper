cd810a4bdcb1ed726c289b179a6d02786b649fa2
KIM JINWOO <rlawlsdn263@naver.com>
2022-05-23 17:15:13 +0900

    Color Flipper: Answer

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const colors = ['#FFE3A9', '#FFC3C3', '#FF8C8C',]
 
-
 const body = document.querySelector('body');
 const backgroundColor = document.querySelector('.backgroundColor'); 
 const btn = document.querySelector('.btn');
@@ -12,6 +11,36 @@ function randomColorHandler() {
     colorCode.innerText = `${randomColor}`;
     colorCode.style.color = `${randomColor}`;
 };
+function randomHexColorHandler() {
+    const randomHexColor = Math.floor(Math.random()*16777215).toString(16);
+    body.style.backgroundColor = `#${randomColor}`;
+    colorCode.innerText = `#${randomColor}`;
+    colorCode.style.color = `#${randomColor}`;
+    console.log(randomHexColor);
+}
 
 randomColorHandler();
-btn.addEventListener('click', randomColorHandler);
+
+const colorFlipper = document.querySelector('.colorFlipper');
+const simpleHex = document.querySelector('.simpleHex');
+
+if(colorFlipper.classList.contains('bold')) {
+    btn.addEventListener('click', randomColorHandler);
+    btn.removeEventListener('click', randomHexColorHandler);
+}
+
+if(simpleHex.classList.contains('bold')) {
+    btn.addEventListener('click', randomHexColorHandler)
+    btn.removeEventListener('click', randomColorHandler);
+}
+
+simpleHex.addEventListener('click', () => {
+    colorFlipper.classList.remove('bold');
+    simpleHex.classList.add('bold');
+
+});
+
+colorFlipper.addEventListener('click', () => {
+    colorFlipper.classList.add('bold');
+    simpleHex.classList.remove('bold');
+})
